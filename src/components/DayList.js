a556d8acf9e3c640fefb31f2bf0c75c2ed748bfe
ChrisnNg <cristopherng@hotmail.com>
2019-09-30 17:20:16 -0700

    list individual days and list all together

---
+++ src/components/DayList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import DayListItem from "components/DayListItem"
+import ReactDOM from "react-dom";
+// Our DayList component will take in three props.
+
+// days:Array a list of day objects (each object includes an id, name, and spots)
+// day:String the currently selected day
+// setDay:Function accepts the name of the day eg. "Monday", "Tuesday"
+
+export default function DayList(props) {
+  const days = props.days.map(day => {
+    return (
+      <DayListItem
+        key={day.id}
+        name={day.name} 
+        spots={day.spots} 
+        selected={day.name === props.day}
+        setDay={props.setDay}  
+        />
+    )
+  })
+  return <ul>{days}</ul>
+}
+
+ReactDOM.render(<DayList days={DayListItem} />, document.getElementById("root"));
---
+++ src/components/DayListItem.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "components/DayListItem.scss";
+
+import classnames from 'classnames';
+
+export default function DayListItem(props) {
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": (props.spots === 0)
+  });
+  const formatSpots = function(props) {
+    if (props.spots === 0) {
+      return <h3 className="text--light">no spots remaining</h3>
+    }
+    if (props.spots === 1) {
+      return <h3 className="text--light">{props.spots} spot remaining</h3>
+    }
+    if (props.spots > 1) {
+      return <h3 className="text--light">{props.spots} spots remaining</h3>
+    }
+  };
+
+  return (
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className={dayClass}>{props.name}</h2> 
+      {formatSpots(props)}
+    </li>
+  );
+}
+
+// TODO: Write a function called formatSpots in the DayListItem component that will format the props.spots to make our tests pass.
+
