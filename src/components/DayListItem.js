ba7ee4647b5ff355d9a00038045558319f30223b
ChrisnNg <cristopherng@hotmail.com>
2019-09-30 20:02:28 -0700

    fixed class names in daylistitem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,24 +6,24 @@ import classnames from 'classnames';
 export default function DayListItem(props) {
   const dayClass = classnames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full": (props.spots === 0)
+    "day-list__item--full": props.spots === 0
   });
   const formatSpots = function(props) {
     if (props.spots === 0) {
-      return <h3 className="text--light">no spots remaining</h3>
+      return 'no spots remaining';
     }
     if (props.spots === 1) {
-      return <h3 className="text--light">{props.spots} spot remaining</h3>
+      return props.spots + ' spot remaining';
     }
     if (props.spots > 1) {
-      return <h3 className="text--light">{props.spots} spots remaining</h3>
+      return props.spots + ' spots remaining';
     }
   };
 
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className={dayClass}>{props.name}</h2> 
-      {formatSpots(props)}
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props)}</h3> 
     </li>
   );
 }
