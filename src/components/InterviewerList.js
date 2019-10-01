909310decb3863d46ca69948c98558c1b9944b0d
ChrisnNg <cristopherng@hotmail.com>
2019-09-30 21:17:07 -0700

    implement display list

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,31 @@
+
+import React from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
+
+// ? Our InterviewerListItem takes in the following props:
+// ? id:number - the id of the interviewer
+// ? name:string - the name of the interviewer
+// ? avatar:url - a url to an image of the interviewer
+// ? selected:boolean - to determine if an interview is selected or not
+// ? setInterviewer:function - sets the interviewer upon selection
+
+export default function InterviewerList(props) {
+
+  const interviewers = props.interviewers.map(interviewer => {
+    console.log('within map', interviewer)
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.name === props.name}
+        setInterviewer={props.setInterviewer}  
+      />
+    )
+  })
+ return <section className="interviewers">
+ <h4 className="interviewers__header text--light">Interviewer</h4>
+ <ul className="interviewers__list">{interviewers}</ul>
+ </section>
+}
