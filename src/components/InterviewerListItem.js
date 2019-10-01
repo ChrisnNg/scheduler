ca8433af49703e79b8101066d9a90b572e251d76
ChrisnNg <cristopherng@hotmail.com>
2019-09-30 20:45:38 -0700

    display interviewer name if selected & sets class as so

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,34 @@
+// ? Our InterviewerListItem takes in the following props:
+// ? id:number - the id of the interviewer
+// ? name:string - the name of the interviewer
+// ? avatar:url - a url to an image of the interviewer
+// ? selected:boolean - to determine if an interview is selected or not
+// ? setInterviewer:function - sets the interviewer upon selection
+
+import React from "react";
+import "components/InterviewerListItem.scss";
+
+import classnames from 'classnames';
+
+export default function InterviewerListItem(props) {
+  const interviewerClass = classnames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+  const displayName = function(props) {
+    if (props.selected) {
+      console.log(props)
+      return props.name;
+    }
+  }
+
+  return (
+  <li className={interviewerClass}>
+    <img
+      className="interviewers__item-image"
+      src="https://i.imgur.com/LpaY82x.png"
+      alt="Sylvia Palmer"
+    />
+   {displayName(props)}
+  </li>
+  );
+}
