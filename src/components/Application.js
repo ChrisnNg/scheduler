76c87635c2f6ff792c8c623ce201ef176862572f
ChrisnNg <cristopherng@hotmail.com>
2019-09-30 20:26:47 -0700

    selecting days now operational

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,5 @@
 
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
 
@@ -24,6 +24,8 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [day, setDay] = useState('Monday');
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -35,8 +37,8 @@ export default function Application(props) {
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu"><DayList
   days={days}
-  day={"Monday"}
-  setDay={day => console.log(day)}
+  day={day}
+  setDay={day => setDay(day)}
 /></nav>
 <img
   className="sidebar__lhl sidebar--centered"
