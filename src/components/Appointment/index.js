beeccb382c88ec6408cd5b8fa654e4bf48b82f63
ChrisnNg <cristopherng@hotmail.com>
2019-10-01 22:57:23 -0700

    complete appointment empty and booked components & stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,18 @@
 import "components/Appointment/styles.scss"
 import React from "react"
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
-export default function Appointment() {
-  return <article className="appointment"></article>
+export default function Appointment(props) {
+  return (
+
+  <article className="appointment">
+    <Header 
+    time={props.time}
+    />
+    {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name} /> : <Empty /> }
+  </article>
+  )
 
 }
