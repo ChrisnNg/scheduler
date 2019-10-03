9328131cb55f7a87cce79b0fe9580fdcb19d67a5
ChrisnNg <cristopherng@hotmail.com>
2019-10-02 17:07:27 -0700

    selectors functional and tests passed

---
+++ src/helpers/selectors.js
@@ -0,0 +1,21 @@
+import { statements } from "@babel/template";
+
+export function getAppointmentsForDay(state, day) {
+  const filteredDays = state.days.filter(days => days.name === day)
+  const result = [];
+  if (filteredDays.length === 0) {
+    return result;
+  }
+
+  const dayAppointments = filteredDays[0].appointments;
+
+  for (let keys in state.appointments) {
+    for(let index of dayAppointments) {
+      if (state.appointments[keys].id === index) {
+        result.push(state.appointments[keys])
+      }
+    }
+  }
+
+  return result;
+}
