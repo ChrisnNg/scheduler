1b9659e9741788ef80259b589290df88062cde81
ChrisnNg <cristopherng@hotmail.com>
2019-10-03 10:34:52 -0700

    get interview & test created and updated

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,5 @@
 
-export default function getAppointmentsForDay(state, day) {
+export function getAppointmentsForDay(state, day) {
   const filteredDays = state.days.filter(days => days.name === day)
   const result = [];
   if (filteredDays.length === 0) {
@@ -15,6 +15,44 @@ export default function getAppointmentsForDay(state, day) {
       }
     }
   }
-
   return result;
 }
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+  let newInterview = {...interview}
+  console.log('newint', newInterview.interviewer)
+  for(let keys in state.interviewers) {
+    if (state.interviewers[keys].id === newInterview.interviewer) {
+      newInterview.interviewer =  state.interviewers[keys]
+      console.log('newinterview result', newInterview)
+      return newInterview;
+    }
+  }
+}
+
+// export function getInterview(state, interview) {
+//   let result = {};
+//     for (let keys in state.appointments) {
+//       if (state.appointments[keys].interview) {
+//         console.log("getInterview", state.appointments[keys].interview)
+//         result = {
+//           "student": state.appointments[keys].interview.student,
+//           "interviewer": {  
+//             "id": state.appointments[keys].interviewer,
+//             "name": state.interviewers[state.appointments[keys].interviewer].name,
+//             "avatar": state.interviewers[state.appointments[keys].interviewer].avatar
+//           }
+//         }
+  
+//       } else {
+//         console.log(`null detected at key=${keys}`)
+//         return null
+//       }
+//     }
+//     console.log('getinterview result', result)
+//     return result
+//   }
+  
