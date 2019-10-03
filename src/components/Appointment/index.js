2d6fee04b512b29d6c64d193348f4c75e7d0b001
ChrisnNg <cristopherng@hotmail.com>
2019-10-03 14:09:29 -0700

    implement transitions to create appt and cancel appt

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,16 +3,54 @@ import React from "react"
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import useVisualMode from "hooks/useVisualMode.js"
+import Form from "components/Appointment/Form.js"
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE"
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  
+    
+  const onAdd = function(){
+    transition(CREATE)
+  }
+  const onCancel = function(){
+    back()
+  }
   return (
 
   <article className="appointment">
     <Header 
     time={props.time}
     />
-    {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name} /> : <Empty /> }
+  {mode === EMPTY && 
+  <Empty 
+  onAdd={onAdd} 
+  />}
+  {mode === SHOW && (
+  <Show
+    student={props.interview.student}
+    interviewers={props.interview.interviewer}
+  />
+)}  
+  {mode === CREATE && (
+  <Form
+  interviewers={[]}
+  onCancel={onCancel}
+  />
+)}  
   </article>
   )
 
 }
+// ? The Form component should take the following props:
+
+// ? name:String
+// ? interviewers:Array
+// ? interviewer:Number
+// ? onSave:Function
+// ? onCancel:Function
