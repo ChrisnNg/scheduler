9dce0c20a346b9cd7b4b8923bd2a6015c53c378c
ChrisnNg <cristopherng@hotmail.com>
2019-10-03 13:09:38 -0700

    custom hooks to transition and back & tests complete

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react"
+
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = function(mode, replace = false) {
+    setMode(mode)
+    setHistory([...history, mode])
+    if (replace) {
+      setHistory(history.splice(history.length - 2, 1, mode))
+    }
+  }
+  const back = function() {
+    if (history.length > 1) {
+      history.pop()
+    }
+    setMode(history[history.length-1])
+  }
+
+  return { mode, transition, back };
+}
+
+// take in an initial mode
+// set the mode state with the initial mode provided
+// return an object with a property mode
