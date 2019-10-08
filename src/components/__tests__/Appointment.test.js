c2bde80842b352cb1b7a1af5a773020a9beb7390
ChrisnNg <cristopherng@hotmail.com>
2019-10-08 16:22:08 -0700

    remove application test that was present

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -12,15 +12,10 @@ import { render } from "@testing-library/react";
 /*
   We import the component that we are testing
 */
-import Application from "components/Application";
 import Appointment from "components/Appointment/index";
 /*
   A test that renders a React Component
 */
-it("renders without crashing", () => {
-  render(<Application />);
-});
-
 describe("Appointment", () => {
   it("renders without crashing", () => {
     render(<Appointment />);
